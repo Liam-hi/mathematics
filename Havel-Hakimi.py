111a992288b827aeb8faccbc9520d3df94ae8a9a
Nima Rahimian <74251943+Nima-byte@users.noreply.github.com>
2021-09-25 21:14:30 +0200

    Create Havel-Hakimi.py

---
+++ Havel-Hakimi.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 22 13:50:05 2021
+
+@author: Nima Rahimian
+"""
+
+
+print("Havel-Hakimi algorithm")
+
+
+"""
+References:
+
+S. Hakimi, On the realizability of a set of integers as degree sequences of the vertices of a graph, 
+J. SIAM Appl. Math. 10 (1962), 496–506.
+
+E. Triesch, Degree sequences of graphs and dominance order, 
+J. Graph Theory 22 (1996), no. 1, 89–93.
+  
+"""
+
+def Havel(n):
+    
+    step = len(n)
+    
+    count = sum(map(lambda x : x % 2 == 1, n))
+ 
+    for i in range(step):
+        if n[i] >= len(n) or n[i] > 2 * (len(n) -1) or n[i] < 0 or count % 2 == 1:
+            return False
+            break;
+    else:
+        
+        return True
+
+def Hakimi(sequence):
+    
+    sequence.sort()
+    sequence.reverse()
+    
+    array, step, min_idx = [], sequence[0], len(sequence)
+    
+    if min_idx > 1 and Havel(sequence) == True:
+        
+        for i in range(step):
+            array.append(sequence[1:][i] - 1)
+            
+            
+        if len(array) == 1:
+            sequence[1] = array[0]
+            print(sequence[1:])
+            
+            return Hakimi(sequence[1:])
+        
+        else: 
+            sequence[1:len(array) + 1] = array
+            print(sequence[1:])
+            
+            return Hakimi(sequence[1:])
+        
+    else: 
+        
+        return Havel(sequence)
+
+# (Hakimi([9, 8, 8, 8, 6, 5, 4, 4, 2, 2]))
+# print(Hakimi([3, 3, 1, 1, 1, 1, 1, 1]))
+# print(Hakimi([2, 2, 2]))
+# print(Hakimi([3, 3, 3, 1, 2]))
+# print(Hakimi([6, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1]))
+print(Hakimi([8, 1]))
