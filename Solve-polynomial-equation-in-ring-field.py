5fa63ec1de45cd61919d4ff4e7b142407c13b149
Nima Rahimian <74251943+Nima-byte@users.noreply.github.com>
2022-02-23 14:16:12 +0100

    Create Solve-polynomial-equation-in-ring-field.py

---
+++ Solve-polynomial-equation-in-ring-field.py
@@ -0,0 +1,44 @@
+def root(eqn, x, y):
+    eqnClean = eqn.replace("x", "*" + str(y))
+    str1 = " "
+    
+    for i in range(len(eqnClean)):
+        if eqnClean[i] == "*" and eqnClean[i - 1] == " ":
+            str1 += ""
+        elif eqnClean[i] == '^':
+            str1 += "**"    
+        else:
+            str1 += eqnClean[i]
+            
+    if str1[1] == "*":
+        return eval(str1[2:len(str1)]) % x
+    else:
+        return eval(str1[1:len(str1)]) % x
+
+def poly(a, b):
+    """Faktoruppdelar en ring/kropp/grupp. 
+
+    ----------
+    a : string
+     representerar polynomet
+    b : int
+     representerar en grupp, ring eller kropp.
+    Returns
+    -------
+    list :
+     listan innehåller muterbara objekt som representerar polynomets rötter.
+    Examples
+    --------
+    >>> (poly("x^5 + 2x^4 + 2x^2 + 1", 3)
+    ([2, 4])
+    """
+    l = []
+    for i in range(b):
+        if root(a, b, i) == 0:
+            l.append(i)
+    
+    return l
+        
+    
+# print(poly("x^5 + 2x^4 + 2x^2 + 1", 3))
+# print(poly("x^2 + 4x + 3", 5))
