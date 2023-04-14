fe906a5483b2e4319d4c141ea132a27ff299adf7
Liam Rahimian <74251943+Liam-hi@users.noreply.github.com>
2023-04-14 14:06:36 +0200

    init

--- speed-optimisation.py
+++ speed-optimisation.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 16 19:33:13 2021
-
-@author: nimarahimian
-"""
-
-import time
-
 print("Problem 1")
+
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
